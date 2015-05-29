e9a25fdb32e0d1eb18875c3d1fb1f8ec483bddc8
Thijs <Thijs.driessen44@gmail.com>
2015-05-29 15:59:08 +0200

    Revised Roam Database
    database without profiel_posts & profiel_reviews
    posts & reviews direct linked to account

--- roam.sql
+++ roam.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: 127.0.0.1
--- Gegenereerd op: 29 mei 2015 om 13:46
+-- Gegenereerd op: 29 mei 2015 om 15:57
 -- Serverversie: 5.6.17
 -- PHP-versie: 5.5.12
 
@@ -86,10 +86,12 @@ CREATE TABLE IF NOT EXISTS `faciliteiten_etablissement` (
 
 CREATE TABLE IF NOT EXISTS `posts` (
   `post_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `klant_ID` int(11) NOT NULL,
   `post_datum` date NOT NULL,
   `post_titel` varchar(255) NOT NULL,
   `post_inhoud` text NOT NULL,
-  PRIMARY KEY (`post_ID`)
+  PRIMARY KEY (`post_ID`),
+  KEY `klant_ID` (`klant_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -112,48 +114,20 @@ CREATE TABLE IF NOT EXISTS `profiel` (
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `profiel_posts`
---
-
-CREATE TABLE IF NOT EXISTS `profiel_posts` (
-  `row_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `profiel_ID` int(11) NOT NULL,
-  `post_ID` int(11) NOT NULL,
-  PRIMARY KEY (`row_ID`),
-  KEY `profiel_ID` (`profiel_ID`),
-  KEY `post_ID` (`post_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `profiel_reviews`
---
-
-CREATE TABLE IF NOT EXISTS `profiel_reviews` (
-  `row_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `profiel_ID` int(11) NOT NULL,
-  `review_ID` int(11) NOT NULL,
-  PRIMARY KEY (`row_ID`),
-  KEY `profiel_ID` (`profiel_ID`),
-  KEY `review_ID` (`review_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
 -- Tabelstructuur voor tabel `reviews`
 --
 
 CREATE TABLE IF NOT EXISTS `reviews` (
   `review_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `klant_ID` int(11) NOT NULL,
   `review_inhoud` text NOT NULL,
   `review_titel` varchar(255) NOT NULL,
   `review_beoordeling` float NOT NULL,
   `review_datum` date NOT NULL,
   `faciliteit_ID` int(11) NOT NULL,
   PRIMARY KEY (`review_ID`),
-  KEY `faciliteit_ID` (`faciliteit_ID`)
+  KEY `faciliteit_ID` (`faciliteit_ID`),
+  KEY `klant_ID` (`klant_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
@@ -168,29 +142,22 @@ ALTER TABLE `faciliteiten_etablissement`
   ADD CONSTRAINT `faciliteiten_etablissement_ibfk_2` FOREIGN KEY (`etablissement_ID`) REFERENCES `etablissement` (`etablissement_ID`);
 
 --
+-- Beperkingen voor tabel `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`klant_ID`) REFERENCES `account` (`klant_ID`);
+
+--
 -- Beperkingen voor tabel `profiel`
 --
 ALTER TABLE `profiel`
   ADD CONSTRAINT `profiel_ibfk_1` FOREIGN KEY (`klant_ID`) REFERENCES `account` (`klant_ID`);
 
 --
--- Beperkingen voor tabel `profiel_posts`
---
-ALTER TABLE `profiel_posts`
-  ADD CONSTRAINT `profiel_posts_ibfk_1` FOREIGN KEY (`profiel_ID`) REFERENCES `profiel` (`profiel_ID`),
-  ADD CONSTRAINT `profiel_posts_ibfk_2` FOREIGN KEY (`post_ID`) REFERENCES `posts` (`post_ID`);
-
---
--- Beperkingen voor tabel `profiel_reviews`
---
-ALTER TABLE `profiel_reviews`
-  ADD CONSTRAINT `profiel_reviews_ibfk_1` FOREIGN KEY (`profiel_ID`) REFERENCES `profiel` (`profiel_ID`),
-  ADD CONSTRAINT `profiel_reviews_ibfk_2` FOREIGN KEY (`review_ID`) REFERENCES `reviews` (`review_ID`);
-
---
 -- Beperkingen voor tabel `reviews`
 --
 ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`klant_ID`) REFERENCES `account` (`klant_ID`),
   ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`faciliteit_ID`) REFERENCES `faciliteiten` (`faciliteit_ID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
