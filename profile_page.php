158e58e8b93513d866cca87c5c9a2d360d9c9e02
Thijs <Thijs.driessen44@gmail.com>
2015-05-31 10:41:07 +0200

    Dynamic profile_page.php
    Dynamic profile_page.php with static content for placing database queries

--- profile_page.php
+++ profile_page.php
@@ -20,7 +20,26 @@ include 'header.php';
 
 /*	Main document functionality						*/ ?>
 <div id="profile_page">
-	
+	<div id="profile_page">
+	<div id="top">
+		<img src="img/pieter.jpg" class="pieter" alt="Profile Picture">
+		<img src="img/world_map.jpg" class="world_map" alt="Visited countries">
+		<h3>Pieter de graaf</h3>
+		<p>
+			The Netherlands <br>
+			11-09-1992 <br>
+			Sex: Male 
+		</p>
+	</div>
+	<div id="bottom">
+		<div id="current_location">
+			<h3>Current location: Rome, Italy</h3>
+			<img src="img/rome.png" class="rome" alt="current location">
+		</div>
+		<h3>About me</h3>
+		<p>I am Pieter de Graaf, a travelling student. Look at my current location and contact me if you are nearby!</p>
+	</div>
+</div>
 </div>
 <?php
 /*	Importing of the site-wide footer 				*/
